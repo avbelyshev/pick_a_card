3d52236f416943df9c78e60981308470442ef0f0
avbelyshev <32143234+avbelyshev@users.noreply.github.com>
2017-09-20 23:37:05 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -12,4 +12,5 @@ values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
+puts "Card:"
 puts "#{values.sample} of #{suits.sample}"
